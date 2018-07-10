3ce012ff3dc612a91c77c5553b5e746418fae1e9
Kiarash Golezardi <golezardi@ce.sharif.edu>
2018-07-10 12:58:41 +0430

    Add more tests to rss reading

--- src/main/java/com/sahab/nimbo/dose/rss/ReadTest.java
+++ src/main/java/com/sahab/nimbo/dose/rss/ReadTest.java
@@ -1,15 +1,24 @@
 package com.sahab.nimbo.dose.rss;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class ReadTest {
-    public static void main(String[] args) {
-        RSSFeedParser parser = new RSSFeedParser(
-                "http://www.vogella.de/article.rss");
+    public static void main(String[] args) throws IOException {
+                RSSFeedParser parser = new RSSFeedParser(
+                "https://www.isna.ir/rss");
         Feed feed = parser.readFeed();
         System.out.println(feed);
-        for (FeedMessage message : feed.getMessages()) {
+        for (FeedMessage message: feed.getMessages()) {
             System.out.println(message);
 
+            URL url = new URL(message.link);
+            String out = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A").next();
+            InputStream in = url.openStream();
+            System.out.println(out);
         }
-
     }
 }
