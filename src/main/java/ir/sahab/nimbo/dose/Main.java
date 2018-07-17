bcb6a6f953f774861ff9dfb7afff863fa6407d6b
Kiarash Golezardi <golezardi@ce.sharif.edu>
2018-07-17 17:20:46 +0430

    Disable c3p0 logging

--- src/main/java/ir/sahab/nimbo/dose/Main.java
+++ src/main/java/ir/sahab/nimbo/dose/Main.java
@@ -6,10 +6,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: how to do this in a config file?
-        System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "WARNING");
-        System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
-
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(10);
         executor.submit(ConsoleInterface.getInstance());
         executor.scheduleWithFixedDelay(new Updater(executor), 1, 10, TimeUnit.SECONDS);
